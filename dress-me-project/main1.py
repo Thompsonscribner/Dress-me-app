e019988da384edacc5514473167f17c40464a5f3
Thompson Scribner <thompsonscribner@gmail.com>
2024-10-08 20:50:17 -0500

    adding my new files, main1.py and plan.txt

---
+++ dress-me-project/main1.py
@@ -0,0 +1,83 @@
+#------------import statements
+import sqlite3
+
+
+
+#initialize database:
+conn = sqlite3.connect("dressme.sqlite")
+cur = conn.cursor()
+
+
+#------------function definitions
+def generate_outfits():
+    pass
+
+def pick_new():
+    pass
+
+def wear_it():
+    pass
+
+def do_laundry():
+    pass
+
+def wash_1():
+    pass
+
+def wear_1():
+    pass
+
+def declutter_1():
+    pass
+
+def suggest_5():
+    pass
+
+def declutter_all():
+    cur.executescript('''
+    DROP TABLE IF EXISTS Wardrobe;
+    
+    CREATE TABLE Wardrobe (
+        id  INTEGER PRIMARY KEY NOT NULL UNIQUE,
+        Description    TEXT UNIQUE,
+        Clean   BOOLEAN DEFAULT TRUE, 
+        Type   TEXT,
+        Fall    BOOLEAN DEFAULT FALSE,
+        Winter    BOOLEAN DEFAULT FALSE,
+        Spring    BOOLEAN DEFAULT FALSE,
+        Summer    BOOLEAN DEFAULT FALSE,
+        Dress    BOOLEAN DEFAULT FALSE,
+        Onepiece    BOOLEAN DEFAULT FALSE
+    );
+    ''')
+
+def add_item():
+    d = input("describe item: ")
+    t = input("item type (TOP or BOTTOM): ")
+    f = bool(input("appropriate for fall? (y/n): "))
+    w = bool(input("appropriate for winter? (y/n): "))
+    sp = bool(input("appropriate for spring? (y/n): "))
+    su = bool(input("appropriate for summer? (y/n): "))
+    dr = bool(input("is dress? (y/n): "))
+    o = bool(input("is onepiece? (y/n): "))
+
+    
+    cur.execute('''
+    INSERT OR IGNORE INTO Wardrobe (Description, Clean, Type, Fall, Winter, Spring, Summer, Dress, Onepiece) 
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ? ) ''', (d, True, t, f, w, sp, su, dr, o))
+    conn.commit()
+
+def bool(x):
+    if x == "y": return True
+    else: return False
+
+def add_tag():
+    pass
+
+def edit_tags():
+    pass
+
+def retake_photo():
+    pass
+
+add_item()
